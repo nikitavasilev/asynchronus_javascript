5a8fb2efc2c5662aec847c7b2872f19f0faf8ebf
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-25 22:39:15 +0100

    AJAX calls w/ fetch & promises

---
+++ ajax_fetch_promises.js
@@ -0,0 +1,21 @@
+// Makings AJAX Calls with Fetch and Promises
+
+function getWeather(woeid) {
+  
+  fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+  .then(result => {
+    console.log(result);
+    return result.json();
+  })
+  .then(data => {
+    // console.log(data);
+    const today = data.consolidated_weather[0];
+    console.log(`Temperatures in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`);
+  })
+  .catch(error => {
+    console.log(error);
+  }); 
+}
+
+getWeather(2487956); // SF
+getWeather(44418); // London
