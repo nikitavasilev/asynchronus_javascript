e5bf8d435c562396be602c1dc338382c5f73af07
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-03-25 23:49:57 +0100

    typo

--- ajax_fetch_promises.js
+++ ajax_fetch_promises.js
@@ -4,13 +4,13 @@ function getWeather(woeid) {
   
   fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
   .then(result => {
-    console.log(result);
+    // console.log(result);
     return result.json();
   })
   .then(data => {
     // console.log(data);
     const today = data.consolidated_weather[0];
-    console.log(`Temperatures in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`);
+    console.log(`Temperatures today in ${data.title} stay between ${today.min_temp} and ${today.max_temp}.`);
   })
   .catch(error => {
     console.log(error);
